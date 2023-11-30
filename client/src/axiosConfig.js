1973f9c03cdfe259ce84387f0ef173a26fd50a36
Rahul7858 <aman956727@gmail.com>
2023-11-30 19:54:13 +0530

    ipdates

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,12 +1,7 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: window.location.hostname === 'localhost' || 
-  window.location.hostname === '127.0.0.1' ? 
-  process.env.REACT_APP_API_LOCAL_PATH : 
-  process.env.REACT_APP_API_LIVE_PATH,
-  timeout: 5000,
-
+const instance = axios.create({ 
+  baseURL: process.env.REACT_APP_API_LIVE_PATH
 });
 
 export default instance;
